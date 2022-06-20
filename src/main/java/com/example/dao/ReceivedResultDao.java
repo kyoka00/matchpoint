8250ab973f17462c184f7fae7def43341ff303e3
Maeno <22maeno_kyoka@axiz.co.jp>
2022-06-20 19:47:01 +0900

    ReceivedResult Dao/Entity終わり

--- src/main/java/com/example/dao/ReceivedResultDao.java
+++ src/main/java/com/example/dao/ReceivedResultDao.java
@@ -1,5 +1,12 @@
 package com.example.dao;
 
+import java.util.List;
+
+import com.example.entity.ReceivedResult;
+
 public interface ReceivedResultDao {
-	public List<ReceivedResult> searchReceivedResult 
+	public List<ReceivedResult> search(ReceivedResult receivedResult, String keyword);
+	public int insert (ReceivedResult receivedResult);
+	public int update (ReceivedResult receivedResult);
+	public ReceivedResult findByGameInfoId(ReceivedResult receivedResult);
 }
